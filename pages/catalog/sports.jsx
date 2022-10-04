b82bc51908fb8538db378256277b83e45e20a7bb
Daniel Ward <dward1502@gmail.com>
2022-10-04 11:10:15 -0700

    fixed axios

--- pages/catalog/sports.jsx
+++ pages/catalog/sports.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState, useContext } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 import Hero from '../../components/UI/Hero';
 import Image from 'next/image';
